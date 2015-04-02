e14b0c38ea37a949bdb7477f6d3a816b2d6c1ea5
Chris Jefferies <cjefferies8439@gmail.com>
2015-04-02 17:17:07 -0600

    still working on photo uploads

--- app/add-recipe/addRecipe.js
+++ app/add-recipe/addRecipe.js
@@ -15,7 +15,8 @@ angular.module('myApp.addRecipe', ['ngRoute'])
 
         $scope.recipe = {
             ingredients: [],
-            tags: ["test", "another tag"]
+            //tags: ["test", "another tag"]
+            photo: null
         };
 
         // Add the ingredients to the recipe object we're building
@@ -37,30 +38,53 @@ angular.module('myApp.addRecipe', ['ngRoute'])
         //Add a new recipe, alert the user when it's been created or when there was a problem.
         $scope.addRecipe = function () {
             //var boundary = "---------------------------7da24f2e50046";
-            var fd = new FormData();
-            fd.append("photo", $scope.recipe.photo);
-            fd.append("name", $scope.recipe.name);
-            fd.append("description", $scope.recipe.description);
-            fd.append("directions", $scope.recipe.directions);
-            fd.append("ingredients", $scope.recipe.ingredients);
-            fd.append("tags", $scope.recipe.tags);
 
-            console.log(fd);
+            Restangular.all('add-recipe/').customPOST($scope.recipe).then(function (recipe) {
+                alert("You did it! The recipe you created has an id of " + recipe.id);
+                var fd = new FormData();
+                fd.append("photo", $scope.photo);
 
-            $http.post('http://localhost:8001/recipes/', fd, {
-                headers: {'Content-type': undefined},
-                transformRequest: angular.identity
+                var url = 'http://localhost:8001/recipes/' + String(recipe.id) +'/';
 
-            }).success(function () {
-                $location.path('/recipes');
-            }).error(function (response) {
-                console.log('Error response: ' + response);
-            })
+                $http.patch(url, fd, {
+                    headers: {'Content-type': undefined},
+                    transformRequest: angular.identity
+
+                }).success(function () {
+                    alert("Photo should be uploaded now!");
+                    $location.path('/recipes');
+                }).error(function (response) {
+                    console.log('Error response: ' + response);
+                });
+
+
+            });
+
+
+
+
+
+            //var fd = new FormData();
+            //fd.append("photo", $scope.recipe.photo);
+            //fd.append("name", $scope.recipe.name);
+            //fd.append("description", $scope.recipe.description);
+            //fd.append("directions", $scope.recipe.directions);
+            //fd.append("ingredients", $scope.recipe.ingredients);
+            //fd.append("tags", $scope.recipe.tags);
+            //
+            //$http.post('http://localhost:8001/add-recipe/', fd, {
+            //    headers: {'Content-type': undefined},
+            //    transformRequest: angular.identity
+            //
+            //}).success(function () {
+            //    $location.path('/recipes');
+            //}).error(function (response) {
+            //    console.log('Error response: ' + response);
+            //})
         };
 
         $scope.uploadFile = function (files) {
-            $scope.recipe.photo = files[0];
-            console.log($scope.recipe.photo);
+            $scope.photo = files[0];
         };
 
         $scope.cancel = function () {
